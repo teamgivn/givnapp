f1da7c2d740e7616712b6fa319f178e5dd1c1243
mogensmadsen <mm1105@hotmail.com>
2013-11-23 22:23:05 -0600

    pages

--- app/blueprints/main/views.py
+++ app/blueprints/main/views.py
@@ -11,6 +11,10 @@ main = Blueprint(
 def index():
     return render_template('main/index.html')
 
+@main.route('/about/', methods=['GET', 'POST'])
+def about():
+    return render_template('main/about.html')
+
 @main.route('/favicon.ico')
 def favicon():
     return redirect(url_for('static', filename='img/favicon.ico'))
